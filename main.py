75454b2b452675e3fa6d4927f98a99f347b46650
Nickeniklas <savonhen@arcada.fi>
2025-10-13 14:51:08 +0300

    assault: test run

--- main.py
+++ main.py
@@ -12,9 +12,29 @@ import os
 #gym.register_envs(ale_py) # if auto registration fails
 
 def main():
-    env = gym.make('ALE/Assault-v5')
+    env = gym.make('ALE/Assault-v5', render_mode="human")
     obs, info = env.reset()
     obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
+
+    # debugging
+    #print("Action space:", env.action_space)
+    #print("Observation space:", env.observation_space)
+
+    # test run with random actions
+    episodes = 5
+    for episode in range(1, episodes+1):
+        obs, info = env.reset()
+        done = False
+        score = 0 
+
+        while not done:
+            env.render()
+            action = env.action_space.sample()
+            obs, reward, terminated, truncated, info = env.step(action)
+            done = terminated or truncated
+            score += reward
+        print(f'Episode:{episode}, Score:{score}')
+
     env.close()
 
 # script run
