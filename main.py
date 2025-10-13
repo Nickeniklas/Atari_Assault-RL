221a59e1e31eda4e1cf22d7a1ffb77aae75e2e2a
Nickeniklas <savonhen@arcada.fi>
2025-10-13 23:12:51 +0300

    __main__ small improvements

--- main.py
+++ main.py
@@ -136,9 +136,9 @@ def tune_dqn(env_id="ALE/Assault-v5", n_trials=10, timesteps=5000, seed=42):
     """
     def objective(trial):
         # Suggest hyperparameters
-        learning_rate = trial.suggest_loguniform("learning_rate", 1e-5, 1e-3)
-        gamma = trial.suggest_uniform("gamma", 0.90, 0.999)
-        exploration_fraction = trial.suggest_uniform("exploration_fraction", 0.1, 0.4)
+        learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-3)
+        gamma = trial.suggest_float("gamma", 0.90, 0.999)
+        exploration_fraction = trial.suggest_float("exploration_fraction", 0.1, 0.4)
 
         # Create environment
         env = Monitor(gym.make(env_id, render_mode=None))
@@ -213,63 +213,65 @@ if __name__ == "__main__":
     #print("Testing model...")
     #env = setup_env(render_mode="Human")
     #visualize_trained_agent(env, agent.model, episodes=2)
-    """
-    # OPTUNA tuning on PPO agent
-    print("Tuning new PPO agent hyperparameters with Optuna...")
-    env = setup_env(render_mode=None) 
-    best_params, study = tune_ppo(n_trials=15, timesteps=10_000)
     
-    # Full training with best hyperparameters (longer)
-    best_model = PPO(
-        "CnnPolicy",
-        env,
-        n_steps=best_params["n_steps"],
-        gamma=best_params["gamma"],
-        learning_rate=best_params["learning_rate"],
-        ent_coef=best_params["ent_coef"],
-        gae_lambda=best_params["gae_lambda"],
-        clip_range=best_params["clip_range"],
-        verbose=1
-    )
-    best_model.learn(total_timesteps=100_000)  # much longer training
-    best_model.save("./models/PPO_assault_best")
-    print("Best model trained and saved.")
+    if agent.name == "PPO":
+        # OPTUNA tuning on PPO agent
+        print("Tuning new PPO agent hyperparameters with Optuna...")
+        env = setup_env(render_mode=None) 
+        best_params, study = tune_ppo(n_trials=15, timesteps=10_000)
+        
+        # Full training with best hyperparameters (longer)
+        best_model = PPO(
+            "CnnPolicy",
+            env,
+            n_steps=best_params["n_steps"],
+            gamma=best_params["gamma"],
+            learning_rate=best_params["learning_rate"],
+            #ent_coef=best_params["ent_coef"],
+            #gae_lambda=best_params["gae_lambda"],
+            clip_range=best_params["clip_range"],
+            verbose=1
+        )
+        best_model.learn(total_timesteps=50_000)  # much longer training
+        best_model.save("./models/PPO_assault_best")
+        print("Best model trained and saved.")
 
-    # print and plot results
-    print("Printing results...")
-    print_results(env)
-    print("Plotting results...")
-    plot_results(env)
+        # print and plot results
+        print("Printing results...")
+        print_results(env)
+        print("Plotting results...")
+        plot_results(env)
 
-    env.close()
-    """
-    # OPTUNA tuning on DQN agent
-    print("Tuning new DQN agent hyperparameters with Optuna...")
-    env = setup_env(render_mode=None) 
-    best_params, study = tune_dqn(n_trials=10, timesteps=10_000)
-    
-    # Full training with best hyperparameters (longer)
-    best_model = DQN(
-        "CnnPolicy",
-        env,
-        buffer_size=30_000, # limit buffer size from memory usage
-        learning_starts=3000,
-        learning_rate=best_params["learning_rate"],
-        gamma=best_params["gamma"],
-        exploration_fraction=best_params["exploration_fraction"],
-        verbose=1,
-        tensorboard_log=f"./logs/assault_tensorboard/"
-    )
-    best_model.learn(total_timesteps=50_000)  
-    best_model.save("./models/DQN_assault_best")
-    print("Best model trained and saved.")
+        env.close()
+        
+    elif agent.name == "DQN":
+        # OPTUNA tuning on DQN agent
+        print("Tuning new DQN agent hyperparameters with Optuna...")
+        env = setup_env(render_mode=None) 
+        best_params, study = tune_dqn(n_trials=10, timesteps=10_000)
+        
+        # Full training with best hyperparameters (longer)
+        best_model = DQN(
+            "CnnPolicy",
+            env,
+            buffer_size=30_000, # limit buffer size from memory usage
+            learning_starts=3000,
+            learning_rate=best_params["learning_rate"],
+            gamma=best_params["gamma"],
+            exploration_fraction=best_params["exploration_fraction"],
+            verbose=1,
+            tensorboard_log=f"./logs/assault_tensorboard/"
+        )
+        best_model.learn(total_timesteps=50_000)  
+        best_model.save("./models/DQN_assault_best")
+        print("Best model trained and saved.")
 
-    # print and plot results
-    print("Printing results...")
-    print_results(env)
-    print("Plotting results...")
-    plot_results(env)
+        # print and plot results
+        print("Printing results...")
+        print_results(env)
+        print("Plotting results...")
+        plot_results(env)
 
-    env.close()
+        env.close()
 
-    
+        
